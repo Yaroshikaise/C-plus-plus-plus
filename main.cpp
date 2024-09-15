69909c8639cf1f95c22301d2d616e6450ae57180
Yaroshikaise <144211697+Yaroshikaise@users.noreply.github.com>
2024-09-15 19:21:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "include/tic_tac_toe.hpp"
+
+using namespace std;
+
+int main() {
+    Board board;
+    Player currentPlayer = Player::X;
+    int moves = 0;
+
+    cout << "Bine ai venit la jocul Tic-Tac-Toe!" << endl;
+    while (true) {
+        printBoard(board);
+        int row, col;
+        cout << "Jucătorul " << (currentPlayer == Player::X ? "X" : "O") << ", introduceți rândul și coloana (0, 1, 2): ";
+        cin >> row >> col;
+
+        if (!makeMove(board, row, col, currentPlayer)) {
+            cout << "Mutare invalidă. Încercați din nou." << endl;
+            continue;
+        }
+
+        moves++;
+        Player winner = checkWinner(board);
+        if (winner != Player::NONE) {
+            printBoard(board);
+            cout << "Jucătorul " << (winner == Player::X ? "X" : "O") << " a câștigat!" << endl;
+            break;
+        } else if (moves == 9) {
+            printBoard(board);
+            cout << "Jocul s-a terminat la egalitate!" << endl;
+            break;
+        }
+
+        currentPlayer = (currentPlayer == Player::X) ? Player::O : Player::X;
+    }
+
+    return 0;
+}
